1c18938270f6d7bde3e107552ad1d230a7f939f3
flute <552452006@qq.com>
2014-02-05 18:15:43 +0800

    1

--- conn.php
+++
@@ -1,5 +0,0 @@
-<?
-$conn=@ mysql_connect("w.rdc.sae.sina.com.cn:3307","olx1z5y3w2","40wyjyjw1h2ww0hi045hkymjklxwhh0yji33jhxx")or die("连接错误");
-mysql_select_db("app_ipv4",$conn);
-mysql_query("SET NAMES UTF-8");
-?>
