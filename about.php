90804e93cba1e93536e1c228629e2ea567a26387
flute <552452006@qq.com>
2014-02-05 18:16:51 +0800

    liudixing
    Signed-off-by: flute <552452006@qq.com>

---
+++ about.php
@@ -0,0 +1,42 @@
+﻿<?php include"header.php" ?>
+<div class="span12">
+	<ul class="thumbnails">
+		<li class="span2">
+		</li>
+		<li class="span4">
+			<ul class="nav nav-list bs-docs-sidenav affix-top">
+				<li>
+					<a href="index.php">概览</a>
+				</li>
+				<li>
+					<a href="shuoming.php">制作说明</a>
+				</li>
+				<li>
+					<a href="upload.php">开始制作</a>
+				</li>
+				<li>
+					<a href="code.php">核心代码</a>
+				</li>
+				<li class="active">
+					<a href="about.php">关于</a>
+				</li>
+				<li>
+					<a href="liuyan.php">留言</a>
+				</li>
+			</ul>
+		</li>
+		<li class="span8">
+			<div class="table">
+				<h4 align="center">About</h4><hr>
+				<ul>
+				<li>Author：flute</li><hr>
+				<li>Blog：<a href="www.ldsun.com" target="blank">www.ldsun.com</a></li><hr>
+				<li>Email:<a href="Mailto:552452006@qq.com" target="blank">552452006@qq.com</a></li><hr>
+				<li>Ps:比较简陋粗糙，请多多包涵~有什么好的意见敬请留言，或者伊妹儿我，谢谢。</li><hr>
+				</ul>
+					
+			</div>
+		</li>
+	</ul>
+</div>
+<?php include"footer.php" ?>
---
+++ content.php
@@ -0,0 +1,39 @@
+﻿<div class="span12">
+	<ul class="thumbnails">
+		<li class="span2">
+		</li>
+		<li class="span4">
+			<ul class="nav nav-list bs-docs-sidenav affix-top">
+				<li class="active">
+					<a href="index.php">概览</a>
+				</li>
+				<li>
+					<a href="shuoming.php">制作说明</a>
+				</li>
+				<li>
+					<a href="upload.php">开始制作</a>
+				</li>
+				<li>
+					<a href="code.php">核心代码</a>
+				</li>
+				<li>
+					<a href="about.php">关于</a>
+				</li>
+				<li>
+					<a href="liuyan.php">留言</a>
+				</li>
+			</ul>
+		</li>
+		<li class="span8">
+			<div class="table">	
+				<h3 align="center">ASCII风格图像</h3><hr><br>
+					<p><strong>ASCII</strong>是AmericanStandardCodeforInformationInterchange的缩写。用来制订计算机中每个符号对应的代码，这也叫做计算机的内码(code)。
+					每个ASCII码以1个字节(Byte)储存，从0到数字127代表不同的常用符号，例如大写A的ASCII码是65，小写a则是97。
+					由于ASCII字节的七个位，最高位并不使用，所以后来又将最高的一个位也编入这套内码中，
+					成为八个位的延伸ASCII(ExtendedASCII)码，这套内码加上了许多外文和表格等特殊符号，成为目前常用的内码。</p>
+                       <p>这是一个用字符代码而生成成图片的工具。可以将您的图片转化为ASCII码风格的图像，生成的图像是完全由ASCII码组成的，将图片放大能够清晰的辨认出来
+					生成的图片也可以下载下来作为头像，非常有趣。</p>
+			</div>
+		</li>
+	</ul>
+</div>
